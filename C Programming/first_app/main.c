986364fd6805cee8d47d10776fb4b5fb4130931e
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2023-06-22 13:50:44 +0300

    C

--- C Programming/first_app/main.c
+++ C Programming/first_app/main.c
@@ -10,4 +10,13 @@ int main()
     // And his is single-line
     printf("Hello world!\n");
     return 0;
+
+
+    // VARÝABLES
+
+    // Declare a variable
+    int myNum;
+
+    // Assign a value to the variable
+    myNum = 15;
 }
--- C Programming/first_app/variables.c
+++ C Programming/first_app/variables.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Declare a variable
+    int myNum;
+
+    // Assign a value to the variable
+    myNum = 15;
+    printf("My Number:" + myNum);
+    return 0;
+}
 
