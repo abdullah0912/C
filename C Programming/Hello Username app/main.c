c741040648fedb6fd856e5eb34127c34101bad26
Abdullah Kaşgar <abdullahkasgar2494@gmail.com>
2023-06-25 14:23:22 +0300

    C

---
+++ C Programming/Hello Username app/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Take user's name using Input,
+    then print it on screen like "hello, user"
+*/
+
+
+
+    int main()
+        {
+        char answer = getchar("W");
+        printf("Hello, %s", answer);
+        return 0;
+    }
